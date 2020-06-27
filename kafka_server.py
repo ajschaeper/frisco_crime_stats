567a97a59c8a912140e658df783ca5d50ece078a
Alex Schaeper <alex@schaeper.io>
2020-06-27 15:43:48 +0000

    Add kafka server

--- kafka_server.py
+++ kafka_server.py
@@ -1,23 +1,26 @@
 import producer_server
 
+KAFKA_BROKER_URL = "localhost:9092"
+KAFKA_TOPIC_NAME = "com.udacity.streams.sf_crime_stats.from_json"
+DATA_FILE_PATH = "./police-department-calls-for-service.json"
 
 def run_kafka_server():
-	# TODO get the json file path
-    input_file = ""
+    input_file = DATA_FILE_PATH
 
-    # TODO fill in blanks
     producer = producer_server.ProducerServer(
-        input_file=input_file,
-        topic="",
-        bootstrap_servers="",
-        client_id=""
+        input_file = input_file,
+        topic = KAFKA_TOPIC_NAME,
+        bootstrap_servers = KAFKA_BROKER_URL,
+        client_id="kafka-producer-from-json-file"
     )
 
     return producer
 
 
 def feed():
+    print(f"Starting Kafka producer into broker \"{KAFKA_BROKER_URL}\"")
     producer = run_kafka_server()
+    print(f"Producing data from \"{DATA_FILE_PATH}\" into \"{KAFKA_TOPIC_NAME}\"")
     producer.generate_data()
 
 
