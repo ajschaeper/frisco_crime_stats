d60089ce125ce2c0de2f486e085a5d4248d53a90
Alex Schaeper <alex@schaeper.io>
2020-06-27 14:07:47 +0000

    Add ZooKeeper and Kafka startup in start.sh, along with configs in ./config

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
-
+echo "> Installing Python dependencties with Conda"
 conda install -y --file requirements.txt
 
+echo -n "> Starting ZooKeeper at localhost:2181 ... "
+zookeeper-server-start config/zookeeper.properties 1> /tmp/zookeeper.out 2> /tmp/zookeeper.err &
+sleep 17
+echo "done"
 
+echo -n "> Starting Kafka at localhost:9092 ... "
+kafka-server-start config/server.properties 1> /tmp/kafka.out 2> /tmp/kafka.err &
+echo "done"
+
+# EOF
